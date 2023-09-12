6e76c81b0088cbd8c463a33d4c2df9eedca835a6
Dr-Kubay <kubaystanislav@gmail.com>
2023-09-12 11:16:12 +0300

    Multiples of 3 or 5 (6kyu)

--- Codewars_tasks/Multiples of 3 or 5 (6kyu).py
+++ Codewars_tasks/Multiples of 3 or 5 (6kyu).py
@@ -1,20 +1,8 @@
 def solution(number):
-<<<<<<< HEAD
     num_sum = 0
     for el in range(number):
         if el % 3 == 0 or el % 5 == 0:
             num_sum += el
-=======
-    new_list = []
-    if number <= 0:
-        return 0
-    for el in range(0, number):
-        new_list.append(el)
-    num_sum = 0
-    for n in new_list:
-        if n % 3 == 0 or n % 5 == 0:
-            num_sum += n
->>>>>>> origin/main
     return num_sum
 
 
