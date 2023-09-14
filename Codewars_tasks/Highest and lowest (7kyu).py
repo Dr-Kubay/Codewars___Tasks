6217d75608e03aea531a804d0cd4d0d97b549662
Dr-Kubay <kubaystanislav@gmail.com>
2023-09-14 10:34:08 +0300

    Add tests

--- Codewars_tasks/Highest and lowest (7kyu).py
+++ Codewars_tasks/Highest and lowest (7kyu).py
@@ -5,4 +5,7 @@ def high_and_low(numbers):
     num_list = [int(el) for el in numbers.split()]
     return " ".join([str(max(num_list)), str(min(num_list))])
 
+# tests:
 print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+print(high_and_low("11 22 5 9 1 122"))
+print(high_and_low("83 -5 42 -100 -9 4 7 4 -4  8"))
