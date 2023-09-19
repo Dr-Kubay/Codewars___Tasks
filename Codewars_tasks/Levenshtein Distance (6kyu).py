f56c0f0c195d226eeb65dfb866ce8da8e394e39b
Dr-Kubay <kubaystanislav@gmail.com>
2023-09-19 13:16:28 +0300

    add task

---
+++ Codewars_tasks/Levenshtein Distance (6kyu).py
@@ -0,0 +1,5 @@
+# Your task is to implement a function which calculates the Levenshtein distance for two arbitrary strings.
+
+# Levenshtein Distance, also known as Edit Distance, is used to calculate the difference between two strings.
+
+def levenshtein(a,b)
