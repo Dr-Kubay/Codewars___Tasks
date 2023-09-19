d83422aa39a8db60c4b2c8ec0ae24a58ec99f97a
Dr-Kubay <kubaystanislav@gmail.com>
2023-09-19 13:40:39 +0300

    update task

--- Codewars_tasks/Levenshtein Distance (6kyu).py
+++ Codewars_tasks/Levenshtein Distance (6kyu).py
@@ -8,3 +8,27 @@ def levenshtein(a, b):
     y = len(b)
     d = [[0] * (y + 1) for el in range(x + 1)]
 
+    for el in range(1, x + 1):
+        d[el][0] = el
+
+    for n in range(1, y + 1):
+        d[0][n] = n
+
+    for n in range(1, y + 1):
+        for el in range(1, x + 1):
+            if x[el - 1] == y[n - 1]:
+                cost = 0
+            else:
+                cost = 1
+            d[el][n] = min(d[x - 1][n] + 1,
+            d[el][n - 1] + 1,
+            d[el - 1][n - 1] + cost)
+
+    return d[x][y]
+
+# tests:
+print(levenshtein("lewenstein", "levenshtein"))
+print(levenshtein("algorithm", "rhythm"))
+print(levenshtein("nayvyedosf","sjxen"))
+print(levenshtein("jtbd", "bcx"))
+print(levenshtein("kitten", "sitting"))
