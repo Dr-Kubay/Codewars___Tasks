75e5afe2280f0b1160a6d41fcbc3e1e8e3182757
Dr-Kubay <kubaystanislav@gmail.com>
2023-09-25 16:48:57 +0300

    add task

---
+++ Codewars_tasks/Run-length encoding (6kyu).py
@@ -0,0 +1,18 @@
+# Your task is to write such a run-length encoding. For a given string,
+# return a list (or array) of pairs (or arrays) [ (i1, s1), (i2, s2), …, (in, sn) ]/
+
+from collections import OrderedDict
+def run_length_encoding(s):
+    dict = OrderedDict.fromkeys(s, 0)
+    for el in s:
+        dict[el] += 1
+
+    result = ''
+
+    for key, value in dict.items():
+        result = result + key + str(value)
+    return result
+
+
+# tests:
+print(run_length_encoding("hello world!"))
